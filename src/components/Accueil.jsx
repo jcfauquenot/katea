12ab9b76a05b40a66c79f62e5d33299816f92cf5
jcfauquenot <jcfauquenot@gmail.com>
2022-05-12 15:43:02 +0200

    add route and img in accueil page

---
+++ src/components/Accueil.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import forest from "../assets/natureforest.jpg";
+
+const Accueil = () => {
+  return (
+    <div>
+      <h1>Page accueil</h1>
+      <img className="" src={forest} alt="" />
+    </div>
+  );
+};
+
+export default Accueil;
